d73340c21bb946d9f70e226e0d8c66a38c35e0a9
Geovane Felicio <93933011+geovanefi@users.noreply.github.com>
2022-05-10 20:55:03 -0300

    8

--- projectJavaScript7/coder.js
+++ projectJavaScript7/coder.js
@@ -68,7 +68,7 @@ function resposta6() {
         window.alert (`Adicione as horas para analisar.`);
     }
 }
- function resposta8(){   
+function resposta8(){   
     let meusNumeros = [1,2,3,4,5,6,7]
     for (let index = 0 ; index < meusNumeros.length; index++) {
         let meuValor = meusNumeros[index];
@@ -76,6 +76,8 @@ function resposta6() {
 
         if (resto === 0){
             window.alert (meuValor);
+        } else {
+            window.alert (`falhou.`);
         }
     }
 }
